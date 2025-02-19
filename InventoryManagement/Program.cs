124f440eacc092417726d5492110e1a1a1801fcd
Brenn Santiago <brennaldwin.santiago@gmail.com>
2025-02-19 16:28:19 +0800

    add product class with properties and validation

---
+++ InventoryManagement/Program.cs
@@ -0,0 +1,27 @@
+﻿class Product
+{
+    public int ProductId { get; private set; }
+    public string Name { get; set; }
+    public int QuantityStock { get; set; }
+    public double Price { get; set; }
+
+    public Product(int productId, string name, int quantity, double price)
+    {
+        if (quantity < 0) throw new ArgumentException("Invalid quantity");
+        if (price < 0) throw new ArgumentException("Invalid price");
+
+        ProductId = productId;
+        Name = name;
+        QuantityStock = quantity;
+        Price = price;
+    }
+
+}
+
+class Program
+{ 
+    static void Main()
+    {
+       
+    }
+}
