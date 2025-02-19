fe7a212a6c293439a42a2e00fa1fe308ad9cc9db
Brenn Santiago <brennaldwin.santiago@gmail.com>
2025-02-19 18:19:30 +0800

    add InventoryManager class with methods; add, find, and remove products. Implemented a console-based menu system for user interaction

--- InventoryManagement/Program.cs
+++ InventoryManagement/Program.cs
@@ -17,11 +17,80 @@
     }
 
 }
+class InventoryManager
+{
+    private List<Product> products = new List<Product>();
+    private int productId = 1;
+
+    public Product? FindProduct(int productId)
+    {
+        return products.Find(p => p.ProductId == productId);
+    }
+
+    public void AddProduct(string name, int quantity, double price)
+    {
+        Product product = new Product(productId++, name, quantity, price);
+        products.Add(product);
+    }
+
+    public void RemoveProduct(int productId)
+    {
+        Product? product = FindProduct(productId);
+        if (product == null)
+        {
+            Console.WriteLine("Product not found");
+            return;
+        }
+        products.Remove(product);
+    }
+
+}
 
 class Program
 { 
     static void Main()
     {
-       
+        InventoryManager inventory = new InventoryManager();
+        
+        while (true)
+        {
+            Console.WriteLine("\nInventory Management System in C#");
+            Console.WriteLine("1. Add product");
+            Console.WriteLine("2. Remove product");
+            Console.Write("Choose an option: ");
+
+            if (!int.TryParse(Console.ReadLine(), out int option))
+            {
+                Console.WriteLine("Invalid option");
+                continue;
+            }
+
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Enter Product Name: ");
+                    string name = Console.ReadLine() ?? "Unknown product";
+                    Console.WriteLine("Enter Quantity: ");
+                    int quantity = int.Parse(Console.ReadLine() ?? "0");
+                    Console.WriteLine("Enter Price: ");
+                    double price = double.Parse(Console.ReadLine() ?? "0");
+                    inventory.AddProduct(name, quantity, price);
+                    break;
+
+                case 2:
+                    Console.WriteLine("Enter the Product ID: ");
+                    if (!int.TryParse(Console.ReadLine(), out int id))
+                    {
+                        Console.WriteLine("Invalid Product ID. Please enter a number.");
+                        continue; 
+                    }
+                    inventory.RemoveProduct(id);
+                    break;
+                    
+            }
+
+        }
+
+
     }
 }
